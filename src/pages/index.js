c0ed9965c23c355a23a887ae8680866f5919094f
Kevin David <kevincarld@gmail.com>
2023-05-08 14:43:11 +1000

    added plan for section components

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,15 +4,24 @@ import Layout from "components/util/Layout"
 import { Heading, Button } from "@chakra-ui/react"
 //
 import Container from "components/util/Container"
+import Hero from "components/Hero"
+import Intro from "components/Intro"
+import SuperFit from "components/SuperFit"
+import Banner from "components/Banner"
+import WhatNext from "components/WhatNext"
+import Video from "components/Video"
+import Footer from "components/Footer"
 
 export default function Home() {
   return (
     <Layout>
-      <Container>
-        <Heading as="h1">Hello World</Heading>
-        <Button>Button</Button>
-
-      </Container>
+      <Hero />
+      <Intro />
+      <SuperFit />
+      <Banner />
+      <WhatNext />
+      <Video />
+      <Footer />
     </Layout>
   )
 }
