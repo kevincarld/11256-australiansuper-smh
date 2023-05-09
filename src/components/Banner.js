6274996d684861764809577cff124d17512caf50
Kevin David <kevincarld@gmail.com>
2023-05-09 10:55:04 +1000

    banner mobile layout

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Box, Text, Button, Link, Image, Grid, GridItem } from '@chakra-ui/react'
+import { Box, Text, Button, Link, Image, Grid, GridItem, AspectRatio } from '@chakra-ui/react'
 import PreLine from 'components/util/PreLine'
 import NineLink from 'components/util/NineLink'
 import { mock } from 'utils/common'
@@ -7,20 +7,14 @@ import Container from 'components/util/Container'
 import Img from 'components/util/Img'
 import Figure from 'components/util/Figure'
 import { motion } from 'framer-motion'
+import { animation_zoom } from 'utils/motion'
 
 export default function Banner() {
 
   return (
-    <Box>
-      <h1>I am Banner</h1>
-    </Box>
+    <Figure  overflow='hidden'>
+      <Img display={{base: 'block', lg: 'none'}} dimension='375x655' w='full' mock='banner-1' />
+      <Img display={{base: 'none', lg: 'block'}} dimension='1920x915' w='full' mock='banner-1' />
+    </Figure>
   )
 }
-
-// font styles
-const texts = {
-  xx: {
-
-  }
-}
-
